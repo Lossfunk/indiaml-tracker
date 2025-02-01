a434583000340ca547ce6e0cc9d5350b62e77254
Sohan Basak <ronnie.basak96@gmail.com>
2025-02-02 00:34:35 +0530

    Updated message

--- ui/indiaml-tracker/src/v1/neurips-2024.tsx
+++ ui/indiaml-tracker/src/v1/neurips-2024.tsx
@@ -3,7 +3,7 @@ import { ResearchPapersShowcase } from "./research-paper-showcase";
 export default function Home() {
     return (
       <main className="container mx-auto p-4">
-        <h1 className="text-3xl font-bold mb-8 text-center">Indian Research Papers at NeurIPS and ICML 2024</h1>
+        <h1 className="text-3xl font-bold mb-8 text-center">Indian Research Papers accepted at Top AI/ML conferences in 2024</h1>
         <ResearchPapersShowcase />
       </main>
     )
