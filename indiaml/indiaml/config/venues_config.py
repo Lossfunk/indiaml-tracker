a2e75b15ffd4aefc147b385acee233d8f9f81014
Sohan Basak <ronnie.basak96@gmail.com>
2025-04-03 01:59:07 +0530

    Updated data

--- indiaml/indiaml/config/venues_config.py
+++ indiaml/indiaml/config/venues_config.py
@@ -11,22 +11,22 @@ class VenueConfig(BaseModel):
 
 # Define your venue configurations here
 VENUE_CONFIGS: List[VenueConfig] = [
-    VenueConfig(
-        conference="NeurIPS",
-        year=2024,
-        track="Conference",
-        source_adapter="openreview",
-        source_id="NeurIPS.cc/2024/Conference",
-        adapter_class="NeurIPSAdapter"
-    ),
-    VenueConfig(
-        conference="ICML",
-        year=2024,
-        track="Conference",
-        source_adapter="openreview",
-        source_id="ICML.cc/2024/Conference",
-        adapter_class="ICMLAdapter"
-    ),
+    # VenueConfig(
+    #     conference="NeurIPS",
+    #     year=2024,
+    #     track="Conference",
+    #     source_adapter="openreview",
+    #     source_id="NeurIPS.cc/2024/Conference",
+    #     adapter_class="NeurIPSAdapter"
+    # ),
+    # VenueConfig(
+    #     conference="ICML",
+    #     year=2024,
+    #     track="Conference",
+    #     source_adapter="openreview",
+    #     source_id="ICML.cc/2024/Conference",
+    #     adapter_class="ICMLAdapter"
+    # ),
     VenueConfig(
         conference="ICLR",
         year=2024,
@@ -35,13 +35,13 @@ VENUE_CONFIGS: List[VenueConfig] = [
         source_id="ICLR.cc/2024/Conference",
         adapter_class="ICAIAdapter"
     ),
-    VenueConfig(
-        conference="ICLR",
-        year=2025,
-        track="Conference",
-        source_adapter="openreview",
-        source_id="ICLR.cc/2025/Conference",
-        adapter_class="ICAIAdapter"
-    )
+    # VenueConfig(
+    #     conference="ICLR",
+    #     year=2025,
+    #     track="Conference",
+    #     source_adapter="openreview",
+    #     source_id="ICLR.cc/2025/Conference",
+    #     adapter_class="ICAIAdapter"
+    # )
     # Add more VenueConfig instances as needed
 ]
