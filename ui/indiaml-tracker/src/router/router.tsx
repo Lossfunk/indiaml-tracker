56dc376f8ab8903daae57a1de816c0031e074874
Sohan Basak <ronnie.basak96@gmail.com>
2025-04-09 19:52:52 +0530

    Updated UI

--- ui/indiaml-tracker/src/router/router.tsx
+++ ui/indiaml-tracker/src/router/router.tsx
@@ -10,8 +10,7 @@ import React, { useEffect } from "react";
 const V1Items = {
   V1Homepage: loadable(() => import("../v1/Homepage")),
   Landing: loadable(() => import("../v1/Landing")),
-  NeurIPS2024: loadable(() => import("../v1/neurips-2024")),
-  Maps2024: loadable(() => import("../v1/cc-map")),
+  Papers: loadable(() => import("../v1/papers")),
   About: loadable(() => import("../v1/about")),
   Motivation: loadable(() => import("../v1/motivation")),
 };
@@ -33,7 +32,7 @@ export const router = createBrowserRouter([
     children: [
       {
         path: "papers",
-        element: <V1Items.NeurIPS2024 />,
+        element: <V1Items.Papers />,
       },
       {
         path: "landing",
@@ -46,10 +45,6 @@ export const router = createBrowserRouter([
       {
         path: "motivation",
         element: <V1Items.Motivation />,
-      },
-      {
-        path: "map-2024",
-        element: <V1Items.Maps2024 />,
       },
     ],
   },
