361febc2e7e04a4fc7f9fc9e9adac5ad9efea9ab
Sohan Basak <ronnie.basak96@gmail.com>
2025-07-03 22:34:51 +0530

    deleting unncessary files

--- eda/tweetgen/pipeline/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Tweet Generation Pipeline
-
-A comprehensive system for generating tweet threads from conference data
-with robust checkpointing and resume capabilities.
-"""
-
-__version__ = "1.0.0"
